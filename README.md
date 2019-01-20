1cd31354b9a3114233293df3faffc3a7cca11337
louis030195 <louis.beaumont@gmail.com>
2019-01-20 21:32:00 +0100

    Make it works

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # node-diablo2-data
 expose diablo 2 data in NodeJS
+
+## Features
+
+For example it's often useful to : 
+
+* find areas by id
+* find items by name
+* find monster by name
+
+## Example
+
+```js
+const diabloData=require('node-diablo2-data')('pod_1.13d')
+
+console.log(diabloData.skills[50])
+console.log(diabloData.areas[101])
+console.log(diabloData.npcsByName['Akara'])
+console.log(diabloData.objectsByName['StoneTheta'])
+```
--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const diabloDataToNode = require('./lib/loader')
-const indexer = require('./lib/indexer.js')
 
+const fs = require('fs')
+const csvjson = require('csvjson')
 
-// TODO: csvtojson .txt files ...
-module.exports = function () {
-    return mcDataToNode(diabloData)
+function txtToJson (path, options) {
+  data = fs.readFileSync(path, 'utf8')
+  return csvjson.toObject(data, options)
 }
+
+function loadCsv (path, propertiesDelimiter, customHeaders) {
+  data = fs.readFileSync(path, 'utf8')
+  const lines = data.split('\n')
+  let headers
+  if (customHeaders === undefined) {
+    headers = lines.shift().split('\t')
+  } else {
+    headers = customHeaders
+  }
+  return x = lines
+    .map(line => line.split(propertiesDelimiter))
+    .map(arr => arr.reduce((acc, e, i) => {
+      acc[headers[i]] = e
+      return acc
+    }, {}))
+}
+
+
+module.exports = function (diabloVersion) {
+  const diabloData = {
+    npcs: txtToJson(`data/${diabloVersion}/monster_names.txt`),
+    areas: txtToJson(`data/${diabloVersion}/areas.txt`),
+    skills: loadCsv(`data/${diabloVersion}/skills.txt`, '\t'),
+    objects: loadCsv(`data/${diabloVersion}/objects.txt`, '\t'),
+    itemProperties: loadCsv(`data/${diabloVersion}/item_properties.txt`, '|', ['Name', 'a', 'b', 'c'])
+  }
+
+  return diabloDataToNode(diabloData)
+}
+
