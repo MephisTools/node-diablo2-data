f0fee23e573063d17b734a8191ec6751bb7723d4
louis030195 <louis.beaumont@gmail.com>
2019-06-17 14:22:34 +0200

    Fix comment

--- data/pod_1.13d/item.js
+++ data/pod_1.13d/item.js
@@ -54,8 +54,8 @@ const DirectoryType =
 const ContainerType =
 {
   unspecified: 0,
-  inventory: 2, // TOP is size 10x5 (POD extension)
-  inventory_bottom: 3, // bottom is size 10x4
+  inventory: 2, // TOP is size 10x9 (POD extension)
+  inventory_bottom: 3, // bottom is size 10x1 (just last line ...)
   trader_offer: 4,
   for_trade: 6,
   cube: 8,
